e6314bee3aea4e860f30bdd48dd1d848d5073a0b
zhangtianhu <1071406152@qq.com>
2018-09-18 15:28:24 +0800

    init commit

---
+++ 04-progress/04-std-io.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const asg=['name','email','qq','mobile']
+console.log(msg[0] +':')
+process.stdin.on('data',(data)={
+  console.log(data.slice(0,data.length-1).toString('utf-8'));
+}
